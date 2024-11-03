88f3123017cf2c4a31432d3c7f29e82984de81ab
NameZach <rothwellz@uchicago.edu>
2024-11-02 20:36:11 -0500

    make the threads work

--- meta_share.py
+++ meta_share.py
@@ -40,16 +40,19 @@ def generate_sets():
 
 def get_challenges(month, year):
   url = f"https://www.mtgo.com/decklists/{year}/{0 if month < 10 else ''}{month}?filter=Modern+Challenge"
-  response = requests.get(url)
-  soup = BeautifulSoup(response.text, 'html.parser')
-  decklists = soup.find_all('li', class_='decklists-item')
+  decklist_urls = None
+  while not decklist_urls:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    decklist_urls = soup.find_all('li', class_='decklists-item')
   challenge_urls = []
-  for deck in decklists:
-    link_tag = deck.find('a')
+  for deck_url in decklist_urls:
+    link_tag = deck_url.find('a')
     if link_tag:
         link = link_tag.get('href')
         if "modern-challenge" in link:
           challenge_urls.append(link)
+  print(month, year)
   return challenge_urls
 
 def get_set_of_card(cardname):
@@ -91,42 +94,51 @@ def get_cards_from_deck(deck, filter_cards):
 
 def get_cards_from_challenge(link, filter_cards):
   challenge_individual_cards, challenge_quantity_cards = generate_sets()
-  url = f"https://www.mtgo.com{link}"
-  response = requests.get(url)
+  challenge_url = f"https://www.mtgo.com{link}"
+  response = requests.get(challenge_url)
   soup = BeautifulSoup(response.text, 'html.parser')
   script_tag = soup.find('script', string=re.compile(r'window\.MTGO\.decklists\.data'))
   decklists = None
   if script_tag:
-      json_text = re.search(r'window\.MTGO\.decklists\.data\s*=\s*(\{.*?\});', script_tag.string)
-      if json_text:
-          data = json.loads(json_text.group(1))
+    json_text = re.search(r'window\.MTGO\.decklists\.data\s*=\s*(\{.*?\});', script_tag.string)
+    if json_text:
+        data = json.loads(json_text.group(1))
+        if "decklists" in data:
           decklists = data["decklists"]
+        else:
+          decklists = None
   if decklists:
     for deck in decklists:
       deck_individual_cards, deck_quantity_cards = get_cards_from_deck(deck, filter_cards)
       for key in challenge_individual_cards:
         challenge_individual_cards[key] += deck_individual_cards[key]
         challenge_quantity_cards[key] += deck_quantity_cards[key]
-  return challenge_individual_cards, challenge_quantity_cards
+    return challenge_individual_cards, challenge_quantity_cards
+  else:
+    return None, None
 
-def get_cards_for_month(month, year, filter_cards):
-  challenges = get_challenges(month, year)
+def get_cards_for_month(cur_month, cur_year, filter_cards):
+  challenges = get_challenges(cur_month, cur_year)
+  individual_monthly_data[f"{cur_year}/{cur_month}"] = {}
+  quantity_monthly_data[f"{cur_year}/{cur_month}"] = {}
+  for set_name in set_names_standard:
+    individual_monthly_data[f"{cur_year}/{cur_month}"][set_name] = 0
+    quantity_monthly_data[f"{cur_year}/{cur_month}"][set_name] = 0
   for challenge_link in challenges:
     challenge_individual_cards, challenge_quantity_cards = get_cards_from_challenge(challenge_link, filter_cards)
-    individual_monthly_data[f"{year}/{month}"] = {}
-    quantity_monthly_data[f"{year}/{month}"] = {}
-    for key in challenge_individual_cards:
-      individual_monthly_data[f"{year}/{month}"][key] = challenge_individual_cards[key]
-      quantity_monthly_data[f"{year}/{month}"][key] = challenge_quantity_cards[key]
+    if challenge_quantity_cards:
+      for key in challenge_individual_cards:
+        individual_monthly_data[f"{cur_year}/{cur_month}"][key] += challenge_individual_cards[key]
+        quantity_monthly_data[f"{cur_year}/{cur_month}"][key] += challenge_quantity_cards[key]
+  
+  print(individual_monthly_data[f"{cur_year}/{cur_month}"])
 
   
 def get_cards_over_time(start_month, start_year, end_month, end_year, filter_cards):
   month = start_month
   year = start_year
   threads = []
-  while True:
-    print(month)
-    
+  while True:    
     thread = threading.Thread(target=get_cards_for_month, args=(month, year, filter_cards))
     threads.append(thread)
     thread.start()
@@ -148,15 +160,28 @@ def get_percents(cards):
     cards[key] = int(100 * cards[key] / total)
   return cards
   
-def export_monthly_data(monthly_data, file_name):
+def export_monthly_data(monthly_data, file_name, start_month, start_year, end_month, end_year):
+  month = start_month
+  year = start_year
   with open(file_name, "w") as file:
     file.write("month,mh1,mh2,ltr,mh3,standard\n")
-    for month in monthly_data:
-      file.write(month)
-      percents = get_percents(monthly_data[month])
-      for set_name in set_names_standard:
-        file.write(f",{percents[set_name]}")
-      file.write("\n")
+    while True:    
+
+      month_str = f"{year}/{month}"
+      try:
+        percents = get_percents(monthly_data[month_str])
+        file.write(month_str)
+        for set_name in set_names_standard:
+          file.write(f",{percents[set_name]}")
+        file.write("\n")
+      except:
+        print(month_str)
+      month += 1
+      if month == 13:
+        month = 1
+        year += 1
+      if year > end_year or (month > end_month and year == end_year):
+        break
 
 sets = get_sets_as_sets()
 
@@ -172,5 +197,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     get_cards_over_time(args.start_month, args.start_year, args.end_month, args.end_year, args.filter_cards)
-    export_monthly_data(individual_monthly_data, args.individual_output_file)
-    export_monthly_data(quantity_monthly_data, args.quantity_output_file)
+    export_monthly_data(individual_monthly_data, args.individual_output_file, args.start_month, args.start_year, args.end_month, args.end_year)
+    export_monthly_data(quantity_monthly_data, args.quantity_output_file, args.start_month, args.start_year, args.end_month, args.end_year)
